ba160222d097fe8655c31ce248acd403650272f4
kalanakbr <kalanakr@gmail.com>
2019-03-17 16:57:06 +0530

    Redux Authentication

---
+++ client/src/components/layouts/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default  () => {
+  return (
+    <footer className="bg-dark text-white fixed-bottom mt-5 py-0 text-center">   
+        Copyright &copy; {new Date().getFullYear()} DevConnector
+     </footer>
+  )
+}
